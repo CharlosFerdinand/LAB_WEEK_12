cdcbf4d275c585139ea63803ad822ef6c605ab42
CharlosFerdinand <74392017+CharlosFerdinand@users.noreply.github.com>
2025-11-28 11:01:11 +0700

    Commit No. 3 – Upgrading app with flow

---
+++ app - Copy/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -0,0 +1,21 @@
+package com.example.test_lab_week_12
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "b63fa711c512305f1f5ff10ada88fa3e"
+
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
